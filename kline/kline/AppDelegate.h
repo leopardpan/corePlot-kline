3efd7c8cbfd84b9d70bc553e76fa61465be517f7
leopard <panbaixin@caishuo.com>
2015-03-03 10:58:45 +0800

    creat demo

---
+++ kline/kline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kline
+//
+//  Created by leopard on 15-3-3.
+//  Copyright (c) 2015年 leopard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kline/klineTests/klineTests.m
@@ -0,0 +1,40 @@
+//
+//  klineTests.m
+//  klineTests
+//
+//  Created by leopard on 15-3-3.
+//  Copyright (c) 2015年 leopard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface klineTests : XCTestCase
+
+@end
+
+@implementation klineTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
